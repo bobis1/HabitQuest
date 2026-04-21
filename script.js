a34e6267189cbf9a5604b66583e7b8f53a28e58a
Percy <percygandhi211@gmail.com>
2026-04-21 17:15:43 -0500

    fixed the features

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('download-game-btn').addEventListener('click', function() {
     // Replace with the actual download link or file path
-    const downloadLink = 'https://github.com/bobis1/HabitQuest/releases/tag/Prereleases';
+    const downloadLink = 'https://github.com/bobis1/HabitQuest/releases';
     window.location.href = downloadLink;
 });
